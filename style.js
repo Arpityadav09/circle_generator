6c31dea5405282e809c8c69d6dcdfdb38d433501
Arpityadav09 <arpit.yadav_cs23@gla.ac.in>
2025-07-30 13:25:19 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,39 @@
+const undo=[];
+const redo=[];
+document.body.addEventListener("click",(e)=>{
+    if(e.target.tagName=="BUTTON") return;
+    const a=Math.floor(Math.random()*256);
+const b=Math.floor(Math.random()*256);
+const c=Math.floor(Math.random()*256);
+    let ele=document.createElement("div");
+    ele.className="circle";
+    ele.style.left=`${e.clientX}px`;
+    ele.style.top=`${e.clientY}px`;
+    ele.style.backgroundColor=`rgb(${a},${b},${c})`;
+    document.body.appendChild(ele);
+    undo.push(ele);
+});
+document.querySelector("#undo").addEventListener("click",()=>{
+    if(undo.length>0){
+        const lastcircle=undo.pop();
+        redo.push(lastcircle);
+        lastcircle.remove();
+        console.log(undo.length);
+console.log("redo:"+redo.length)
+    }
+
+});
+document.querySelector("#redo").addEventListener("click",()=>{
+    if(redo.length>0){
+        const ele=redo.pop();
+        document.body.appendChild(ele);
+        undo.push(ele);
+    }
+
+});
+document.querySelector("#reset").addEventListener("click",()=>{
+    while(undo.length>0){
+        undo.pop().remove();
+    }
+    redo.length=0;
+});
